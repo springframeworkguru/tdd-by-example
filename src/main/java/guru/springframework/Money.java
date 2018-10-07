61467fb9ded6e2d31be044e053d8c17c5404a534
John Thompson <john@springframework.guru>
2018-10-07 15:28:30 -0400

    removed subclasses

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -16,13 +16,12 @@ public class Money {
         return currency;
     }
 
-
     public static Money dollar(int amount){
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     public static Money franc(int amount){
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     public boolean equals(Object object) {
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -11,29 +11,21 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    void testMultiplicationDollar() {
+    void testMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
+
+        Money fiveF = Money.franc(5);
+        assertEquals(Money.franc(10), fiveF.times(2));
     }
 
     @Test
     void testEqualityDollar() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(8));
-    }
-
-    @Test
-    void testMultiplicationFranc() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
-    void testEqualityFranc() {
         assertEquals(Money.franc(5), Money.franc(5));
-        assertNotEquals(Money.franc(5), Money.franc(8));
+        assertNotEquals(Money.dollar(5), Money.franc(5));
     }
 
     @Test
