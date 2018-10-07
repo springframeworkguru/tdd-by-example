ce2b52cbba71b5b362398b837e6f55ac9672ffec
John Thompson <john@springframework.guru>
2018-10-07 12:53:24 -0400

    fixing merge mistake

--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -4,7 +4,6 @@ package guru.springframework;
  * Created by jt on 2018-10-05.
  */
 public class Franc extends Money {
-    private int amount;
 
     public Franc(int amount) {
         this.amount = amount;
@@ -12,10 +11,5 @@ public class Franc extends Money {
 
     Franc times( int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    public boolean equals(Object object) {
-        Franc franc = (Franc) object;
-        return amount == franc.amount;
     }
 }
