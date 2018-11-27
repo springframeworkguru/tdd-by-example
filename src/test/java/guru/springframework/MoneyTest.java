9409a0ab47b968bb2f04caa917a5e7fa6d82e03a
Mateusz Bajdak <m.bajdak@AMDC2218.digital.local>
2018-11-27 11:46:47 +0100

    refactor

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -8,30 +8,23 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    void testMultiplicationDollar() {
-        Money five = Money.dollar(5);
-        assertEquals(Money.dollar(10), five.times(2));
-        assertEquals(Money.dollar(15), five.times(3));
+    void testMultiplication() {
+        Money fiveD = Money.dollar(5);
+        assertEquals(Money.dollar(10), fiveD.times(2));
+        assertEquals(Money.dollar(15), fiveD.times(3));
+
+        Money fiveF = Money.frank(5);
+        assertEquals(Money.frank(10), fiveF.times(2));
+        assertEquals(Money.frank(15), fiveF.times(3));
     }
 
     @Test
-    void testEqualityDollar() {
+    void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(8));
-        assertNotEquals(Money.dollar(5), Money.frank(5));
-    }
-
-    @Test
-    void testMultiplicationFrank() {
-        Money five = Money.frank(5);
-        assertEquals(Money.frank(10), five.times(2));
-        assertEquals(Money.frank(15), five.times(3));
-    }
-
-    @Test
-    void testEqualityFrank() {
         assertEquals(Money.frank(5), Money.frank(5));
         assertNotEquals(Money.frank(5), Money.frank(8));
+        assertNotEquals(Money.dollar(5), Money.frank(5));
     }
 
     @Test
