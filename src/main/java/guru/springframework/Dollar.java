439baac6006909b5abc8d89d8bebae601c21e65f
Mateusz Bajdak <m.bajdak@AMDC2218.digital.local>
2018-11-27 10:42:51 +0100

    Dollar impl

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,22 @@
+package guru.springframework;
+
+public class Dollar {
+
+    private int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
+}
---
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -0,0 +1,24 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class MoneyTest {
+
+    @Test
+    void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(new Dollar(10), product);
+        product = five.times(3);
+        assertEquals(new Dollar(15), product);
+    }
+
+    @Test
+    void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
+    }
+}
