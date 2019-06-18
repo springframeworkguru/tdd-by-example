5f4742447f5eea3f0deaaf78e2527e3d76d86f90
venache <Endava!21091991>
2019-06-18 10:36:10 +0300

    TDD by example chapter

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+public class Dollar {
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}
