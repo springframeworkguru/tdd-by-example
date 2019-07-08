c71a16a1b2e75639163bdadd9e1bc1f101b2bc78
garusama <amadeus.garus@pentasys.de>
2019-07-08 23:54:54 +0200

    halloBrille

--- src/main/java/guru/springframework/Bank.java
+++ src/main/java/guru/springframework/Bank.java
@@ -5,4 +5,8 @@ public class Bank {
     public Money reduce(Expression source, String toCurrency){
         return Money.dollar(10);
     }
+
+    public void halloBrille(){
+        System.out.println("Brile");
+    }
 }
