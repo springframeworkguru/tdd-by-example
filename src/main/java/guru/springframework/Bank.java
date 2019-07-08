b44fab8068432360492aeb615c1d4b3dd86b92d9
garusama <amadeus.garus@pentasys.de>
2019-07-09 00:00:55 +0200

    saol

--- src/main/java/guru/springframework/Bank.java
+++ src/main/java/guru/springframework/Bank.java
@@ -5,4 +5,8 @@ public class Bank {
     public Money reduce(Expression source, String toCurrency){
         return Money.dollar(10);
     }
+
+    public Money saol(Expression source, String toCurrency){
+        return Money.dollar(10);
+    }
 }
