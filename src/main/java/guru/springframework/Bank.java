a038548339820afcaa300bdee6e0c52cda998e71
garusama <amadeus.garus@pentasys.de>
2019-07-08 23:59:29 +0200

    reduce2

--- src/main/java/guru/springframework/Bank.java
+++ src/main/java/guru/springframework/Bank.java
@@ -5,4 +5,8 @@ public class Bank {
     public Money reduce(Expression source, String toCurrency){
         return Money.dollar(10);
     }
+
+    public Money reduce2(Expression source, String toCurrency){
+        return Money.dollar(10);
+    }
 }
