e881e10c50a15bf502c64c515b996c89bde99d98
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-11-24 15:30:48 +0100

    Since Dollar and Franc classes now have only constructor calling superconstractor, they have be delated and references to them is changed with references to Money class. Test class made simpler to, duplications removed

--- src/main/java/guru/springframework/Dollar.java
+++
@@ -1,8 +0,0 @@
-package guru.springframework;
-
-public class Dollar extends Money {
-
-    public Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-}
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -8,30 +8,22 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    void testMultiplicationDollar() {
-        Money five = Money.dollar(5);
-        assertEquals(Money.dollar(10), five.times(2));
-        assertEquals(Money.dollar(15), five.times(3));
+    void testMultiplication() {
+        Money fiveDollar = Money.dollar(5);
+        assertEquals(Money.dollar(10), fiveDollar.times(2));
+        assertEquals(Money.dollar(15), fiveDollar.times(3));
+
+        Money fiveFranc = Money.franc(5);
+        assertEquals(Money.franc(10), fiveFranc.times(2));
+        assertEquals(Money.franc(15), fiveFranc.times(3));
     }
 
     @Test
-    void testEqualityDollar() {
+    void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
+        assertEquals(Money.franc(5), Money.franc(5));
         assertNotEquals(Money.dollar(5), Money.dollar(8));
         assertNotEquals(Money.dollar(5), Money.franc(5));
-    }
-
-    @Test
-    void testMultiplicationFranc() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
-    void testEqualityFranc() {
-        assertEquals(Money.franc(5), Money.franc(5));
-        assertNotEquals(Money.franc(5), Money.franc(8));
     }
 
     @Test
