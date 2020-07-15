ea36eb554c31fcab3f9195882b3e234ea64ad7bd
hanseld28 <hansel.donizete@gmail.com>
2020-07-15 18:40:30 -0300

    add: new test method to compare equality of object values

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -15,10 +15,10 @@ public class MoneyTest {
         product = five.times(3);
         assertEquals(15, product.amount);
     }
-
+    
     @Test
     void testEquality() {
-        assertEquals(new Dollar(5), new Dollar(5));
-        assertNotEquals(new Dollar(5), new Dollar(8));
+    	assertEquals(new Dollar(5), new Dollar(5));
+    	assertNotEquals(new Dollar(5), new Dollar(8));
     }
 }
