1ce7d37357642cae4e9af0c15a1a59f261922b35
hanseld28 <hansel.donizete@gmail.com>
2020-07-15 18:38:50 -0300

    add: new override in equals method

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,21 +1,21 @@
 package guru.springframework;
 
 public class Dollar {
-
-    int amount;
-
-    public Dollar(int amount) {
-        this.amount = amount;
-    }
-
-    Dollar times( int multiplier) {
-        return new Dollar(amount * multiplier);
-    }
-    
-    @Override
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        
-        return amount == dollar.amount;
-    }
+	
+	int amount;
+	
+	public Dollar(int amount) {
+		this.amount = amount;
+	}
+	
+	public Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
+	}
+	
+	@Override
+	public boolean equals(Object object) {
+		Dollar dollar = (Dollar) object;
+		
+		return amount == dollar.amount;
+	}
 }
