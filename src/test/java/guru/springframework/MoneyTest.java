4252dc26aa13d4e6f2891a8db7efe02677fa145d
hanseld28 <hansel.donizete@gmail.com>
2020-07-16 14:59:49 -0300

    refactor: method to test multiplication and equality

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -8,29 +8,22 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    void testMultiplicationDollar() {
-        Money five = Money.dollar(5);
-        assertEquals(Money.dollar(10), five.times(2));
-        assertEquals(Money.dollar(15), five.times(3));
+    void testMultiplication() {
+        Money fiveDollars = Money.dollar(5);
+        assertEquals(Money.dollar(10), fiveDollars.times(2));
+        assertEquals(Money.dollar(15), fiveDollars.times(3));
+        
+        Money fiveFrancs = Money.franc(5);
+        assertEquals(Money.franc(10), fiveFrancs.times(2));
+        assertEquals(Money.franc(15), fiveFrancs.times(3));
     }
     
     @Test
-    void testEqualityDollar() {
+    void testEquality() {
     	assertEquals(Money.dollar(5), Money.dollar(5));
     	assertNotEquals(Money.dollar(5), Money.dollar(8));
-    }
-    
-    @Test
-    void testMultiplicationFranc() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-    
-    @Test
-    void testEqualityFranc() {
     	assertEquals(Money.franc(5), Money.franc(5));
-    	assertNotEquals(Money.franc(5), Money.franc(8));
+    	assertNotEquals(Money.dollar(5), Money.franc(5));
     }
     
     @Test
