aad92bcd561dbc555ae8c18037d100ad8d74b3d8
hanseld28 <hansel.donizete@gmail.com>
2020-07-16 14:58:47 -0300

    refactor: remove subclasses and change instaces them on method by superclass

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -18,11 +18,11 @@ public class Money {
 	}
 	
 	public static Money dollar(int amount) {
-		return new Dollar(amount, "USD");
+		return new Money(amount, "USD");
 	}
 	
 	public static Money franc(int amount) {
-		return new Franc(amount, "CHF");
+		return new Money(amount, "CHF");
 	}
 	
 	@Override
