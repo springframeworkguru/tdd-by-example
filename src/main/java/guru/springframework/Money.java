d706337164c45d2e5bb6f5f8bc4f2b316ee77302
hanseld28 <hansel.donizete@gmail.com>
2020-07-16 11:13:47 -0300

    add: new abstract method and build method to each subclass

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,8 +1,18 @@
 package guru.springframework;
 
-public class Money {
+public abstract class Money {
 	protected int amount;
-
+	
+	public abstract Money times(int multiplier);
+	
+	public static Money dollar(int amount) {
+		return new Dollar(amount);
+	}
+	
+	public static Money franc(int amount) {
+		return new Franc(amount);
+	}
+	
 	@Override
 	public boolean equals(Object object) {
 		Money money = (Money) object;
