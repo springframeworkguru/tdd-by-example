17d21bb3ddece7e6a18d64df85e0ec3c17026aae
hanseld28 <hansel.donizete@gmail.com>
2020-07-16 13:51:27 -0300

    update: times and override toString method

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,6 +1,6 @@
 package guru.springframework;
 
-public abstract class Money {
+public class Money {
 	protected int amount;
 	protected String currency;
 
@@ -9,7 +9,9 @@ public abstract class Money {
 		this.currency = currency;
 	}
 	
-	public abstract Money times(int multiplier);
+	public Money times(int multiplier) {
+		return new Money(amount * multiplier, this.currency);
+	}	
 	
 	public String currency() {
 		return currency;
@@ -28,6 +30,14 @@ public abstract class Money {
 		Money money = (Money) object;
 		
 		return amount == money.amount 
-				&& this.getClass().equals(object.getClass());
+				&& this.currency == money.currency;
 	}
+
+	@Override
+	public String toString() {
+		return "Money [amount=" + amount 
+				+ ", currency=" + currency + "]";
+	}
+	
+	
 }
