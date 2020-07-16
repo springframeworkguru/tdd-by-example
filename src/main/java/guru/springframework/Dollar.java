622f263d198dfe168d5d65de72f202754ecd0c51
hanseld28 <hansel.donizete@gmail.com>
2020-07-16 13:52:08 -0300

    change: move times method to superclass

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -5,9 +5,4 @@ public class Dollar extends Money {
 	public Dollar(int amount, String currency) {
 		super(amount, currency);
 	}
-	
-	@Override
-	public Money times(int multiplier) {
-		return Money.dollar(amount * multiplier);
-	}	
 }
--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -5,14 +5,4 @@ public class Franc extends Money {
 	public Franc(int amount, String currency) {
 		super(amount, currency);
 	}
-	
-	@Override
-	public Money times(int multiplier) {
-		return Money.franc(amount * multiplier);
-	}
-
-	@Override
-	public String currency() {
-		return currency;
-	}
 }
