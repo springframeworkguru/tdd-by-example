092be39ab8e5c3a02eda77e370256b4101569842
hanseld28 <hansel.donizete@gmail.com>
2020-07-16 11:53:06 -0300

    add: attribute and new constructor to class

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -2,15 +2,25 @@ package guru.springframework;
 
 public abstract class Money {
 	protected int amount;
+	protected String currency;
+
+	public Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
 	
 	public abstract Money times(int multiplier);
 	
+	public String currency() {
+		return currency;
+	}
+	
 	public static Money dollar(int amount) {
-		return new Dollar(amount);
+		return new Dollar(amount, "USD");
 	}
 	
 	public static Money franc(int amount) {
-		return new Franc(amount);
+		return new Franc(amount, "CHF");
 	}
 	
 	@Override
