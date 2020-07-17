479ce7b2e32bac1be6b2f24da8fd3863c1bb3f4e
hanseld28 <hansel.donizete@gmail.com>
2020-07-17 09:49:30 -0300

    add: new test case to mixed addition

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -83,4 +83,16 @@ public class MoneyTest {
     	assertEquals(1, new Bank().rate("USD", "USD"));
     	assertEquals(1, new Bank().rate("CHF", "CHF"));
     }
+    
+    @Test
+    void testMixedAddition() {
+    	Expression fiveBucks = Money.dollar(5);
+    	Expression tenFrancs = Money.franc(10);
+    	Bank bank = new Bank();
+    	
+    	bank.addRate("CHF", "USD", 2);
+    	Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+    	
+    	assertEquals(Money.dollar(10), result);
+    }
 }
