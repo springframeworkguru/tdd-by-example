a6aceba7425cdffd1a67bb7ba6d45a299097bf3f
hanseld28 <hansel.donizete@gmail.com>
2020-07-17 09:25:05 -0300

    add: new Pair class to represent currency pairs

---
+++ src/main/java/guru/springframework/Pair.java
@@ -0,0 +1,32 @@
+package guru.springframework;
+
+import java.util.Objects;
+
+public class Pair {
+	
+	private final String from;
+	private final String to;
+	
+	public Pair(String from, String to) {
+		this.from = from;
+		this.to = to;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(from, to);
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) return true;
+		if (object == null || getClass() != object.getClass()) return false;
+		Pair pair = (Pair) object;
+		
+		return Objects.equals(from, pair.from) &&
+				Objects.equals(to, pair.to);
+	}
+	
+	
+	
+}
