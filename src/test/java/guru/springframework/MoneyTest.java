83a344833c0439fb439f4e145bbaf135b57dceaa
hanseld28 <hansel.donizete@gmail.com>
2020-07-17 09:32:10 -0300

    add: new test cases to reduce money and rate

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -68,4 +68,19 @@ public class MoneyTest {
     	
     	assertEquals(Money.dollar(1), result);
     }
+    
+    @Test
+    void testReduceMoneyDifferentCurrency() {
+    	Bank bank = new Bank();
+    	bank.addRate("CHF", "USD", 2);
+    	Money result = bank.reduce(Money.franc(2), "USD");
+    	
+    	assertEquals(Money.dollar(1), result);
+    }
+    
+    @Test
+    void testIdentityRate() {
+    	assertEquals(1, new Bank().rate("USD", "USD"));
+    	assertEquals(1, new Bank().rate("CHF", "CHF"));
+    }
 }
