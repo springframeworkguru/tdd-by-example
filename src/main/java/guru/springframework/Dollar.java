6a24572e090fa03199f8d163f03dfd00ca1226c5
Vitor Costa <vitor.costa@redspark.io>
2020-10-01 22:47:21 -0300

    First example of course. Module tdd. working with baby steps dude

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -8,7 +8,7 @@ public class Dollar {
         this.amount = amount;
     }
 
-    void times(int multiplier) {
-        amount *= multiplier;
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 }
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -1,14 +1,16 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MoneyTest {
 
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        Assertions.assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
