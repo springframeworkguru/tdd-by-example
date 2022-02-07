756fecd29cc649b22b8c14a2111f9f8616e1be72
MalloryDuke <mallory.duke@ahead.com>
2022-02-07 11:04:41 -0600

    Created mduke-progress branch to track my progress and added files

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+public class Dollar {
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    void times( int multiplier) {
+        amount *= multiplier;
+    }
+}
