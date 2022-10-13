d5eecf42118281c669c626ab47f6333a0562ec2b
Luis Rosales <guama1239@gmail.com>
2022-10-13 13:34:56 -0500

    Great

---
+++ src/main/java/Foo.java
@@ -0,0 +1,5 @@
+public class Foo {
+    String getMessage(){
+        return "MyMessage";
+    }
+}
---
+++ src/test/java/FooTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FooTest {
+
+    @Test
+    void getMessage() {
+    }
+}
