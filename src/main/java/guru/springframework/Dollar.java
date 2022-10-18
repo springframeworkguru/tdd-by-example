f82323818ca91925283065f1d45bb30b284fb58f
Luis Rosales <guama1239@gmail.com>
2022-10-18 11:23:16 -0500

    Great

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -6,7 +6,12 @@ public class Dollar {
         this.amount = amount;
     }
 
-    void times(int multiplier){
-        amount *= multiplier;
+    Dollar times(int multiplier){
+        return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object){
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
     }
 }
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -3,6 +3,7 @@ package guru.springframework;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
 
@@ -10,6 +11,15 @@ public class MoneyTest {
     void testMultiplication(){
         Dollar five = new Dollar(5);
         five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+
+    @Test
+    void testEquality(){
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
     }
 }
