55a0219b4b12991034a38b10fbe3e7d91313fa38
Luis Rosales <guama1239@gmail.com>
2022-10-18 11:03:23 -0500

    Great

--- src/main/java/Foo.java
+++
@@ -1,5 +0,0 @@
-public class Foo {
-    String getMessage(){
-        return "MyMessage";
-    }
-}
---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+public class Dollar {
+    int amount;
+    public Dollar(int amount){
+        this.amount = amount;
+    }
+
+    void times(int multiplier){
+        amount *= multiplier;
+    }
+}
