5a67b6f101129d7c6bfdaab08b1b0f0c1bbc15ff
Luis Rosales <guama1239@gmail.com>
2022-10-18 19:36:05 -0500

    Refactoring of Dollar and Franc

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -5,7 +5,7 @@ public class Dollar extends Money{
     public Dollar(int amount){
         this.amount = amount;
     }
-
+//
     Dollar times(int multiplier){
         return new Dollar(amount * multiplier);
     }
