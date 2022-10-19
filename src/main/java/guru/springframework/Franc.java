89e7b7d9301f4c488447ccafa2f311d3ca97c59c
Luis Rosales <guama1239@gmail.com>
2022-10-18 20:58:31 -0500

    Refactoring

--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -1,8 +1,6 @@
 package guru.springframework;
 
 public class Franc extends Money{
-//    private int amount;
-
     public Franc(int amount, String currency){
         super(amount, currency);
     }
