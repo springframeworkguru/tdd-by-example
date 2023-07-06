e8bda762eb3debf85bf6aae879c90b133a6d36e2
charliepoker <iheanachocharlie@gmail.com>
2023-07-06 13:34:22 -0400

    refactored greeting test

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,4 +1,29 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GreetingTest {
-  
+    private Greeting greeting;
+
+    @BeforeEach
+    void setUp(){
+        System.out.println("In Before Each.....");
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloworld() {
+
+        System.out.println(greeting.helloworld());
+    }
+
+
+    @Test
+    void helloworld1() {
+
+        System.out.println(greeting.helloworld("charlie"));
+    }
 }
